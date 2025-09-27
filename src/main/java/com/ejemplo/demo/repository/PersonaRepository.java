a78a11ef7f3e918a7e34f31010b50a8dbb1be843
Anthonygs17 <anthony.guimarey@utec.edu.pe>
2025-09-27 15:13:08 -0500

    remove example files

--- src/main/java/com/ejemplo/demo/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplo.demo.repository;
-
-import com.ejemplo.demo.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
